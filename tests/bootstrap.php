7dc4b0b00edd359bfd638b0c2e58d17bcccd69af
Chauncey McAskill <chauncey@mcaskill.ca>
2020-05-04 23:33:42 -0400

    Replace phpunit/phpunit
    With symfony/phpunit-bridge

---
+++ tests/bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * This file is part of the "composer-exclude-files" plugin.
+ *
+ * © Chauncey McAskill <chauncey@mcaskill.ca>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+error_reporting(E_ALL);
+
+if (function_exists('date_default_timezone_set') && function_exists('date_default_timezone_get')) {
+    date_default_timezone_set(@date_default_timezone_get());
+}
+
+require __DIR__.'/../vendor/autoload.php';
